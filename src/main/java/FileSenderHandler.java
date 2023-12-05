8028ce4f5c4dbe6f2e94f737cf63071941a5c165
Özcan <oezcan.karaca@stud.tu-darsmtad.de>
2023-12-05 17:12:57 +0000

    Sender and Receiver classes are also done!

--- src/main/java/FileSenderHandler.java
+++ src/main/java/FileSenderHandler.java
@@ -1,39 +1,41 @@
 import io.netty.channel.*;
-import io.netty.handler.stream.ChunkedFile;
-
 import java.io.File;
 
-public class FileSenderHandler extends SimpleChannelInboundHandler<Channel> {
-
+public class FileSenderHandler extends SimpleChannelInboundHandler<Object> {
     private final String filePath;
+    private final String peerIP; // Hinzufügen der Peer-IP
 
-    public FileSenderHandler(String filePath) {
+    public FileSenderHandler(String filePath, String peerIP) {
         this.filePath = filePath;
-        System.out.println("FileSenderHandler erstellt mit Dateipfad: " + filePath);
+        this.peerIP = peerIP; // Speichern der Peer-IP
     }
 
     @Override
-    public void channelActive(ChannelHandlerContext ctx) throws Exception {
+    public void channelActive(ChannelHandlerContext ctx) {
         File file = new File(filePath);
-        System.out.println("FileSenderHandler: Verbindung aktiv. Datei wird gesendet: " + file.getAbsolutePath());
-        if (file.exists() && !file.isDirectory()) {
-            ctx.writeAndFlush(new ChunkedFile(file)).addListener(ChannelFutureListener.CLOSE);
-            System.out.println("FileSenderHandler: Datei gesendet und Channel wird geschlossen.");
+        if (file.exists()) {
+            System.out.println("Sending file: " + filePath + " to peer " + peerIP);
+            // Senden der Datei und Hinzufügen eines ChannelFutureListeners
+            ctx.writeAndFlush(new DefaultFileRegion(file, 0, file.length())).addListener(new ChannelFutureListener() {
+                @Override
+                public void operationComplete(ChannelFuture future) {
+                    if (future.isSuccess()) {
+                        System.out.println("File sent successfully: " + filePath);
+                    } else {
+                        System.err.println("Error sending file: " + future.cause());
+                    }
+                    ctx.close(); // Schließen des Channels nach dem Senden
+                }
+            });
         } else {
-            System.err.println("FileSenderHandler: Datei existiert nicht oder ist ein Verzeichnis.");
+            System.err.println("File not found: " + filePath);
             ctx.close();
         }
     }
 
     @Override
-    protected void channelRead0(ChannelHandlerContext ctx, Channel msg) throws Exception {
-        // Nichts zu tun hier
-    }
-
-    @Override
-    public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) {
-        System.err.println("FileSenderHandler: Ein Fehler ist aufgetreten: " + cause.getMessage());
-        cause.printStackTrace();
-        ctx.close();
+    protected void channelRead0(ChannelHandlerContext ctx, Object msg) {
+        // Nicht benötigt in diesem Kontext
+        throw new UnsupportedOperationException("Unimplemented method 'channelRead0'");
     }
 }
--- src/main/java/LectureStudioServer.java
+++ src/main/java/LectureStudioServer.java
@@ -1,5 +1,3 @@
-import java.io.File;
-
 import io.netty.bootstrap.ServerBootstrap;
 import io.netty.channel.*;
 import io.netty.channel.nio.NioEventLoopGroup;
@@ -54,42 +52,4 @@ public class LectureStudioServer {
         new LectureStudioServer(port).start();
     }
 
-    class FileSenderHandler extends SimpleChannelInboundHandler<Object> {
-        private final String filePath;
-        private final String peerIP; // Hinzufügen der Peer-IP
-    
-        public FileSenderHandler(String filePath, String peerIP) {
-            this.filePath = filePath;
-            this.peerIP = peerIP; // Speichern der Peer-IP
-        }
-    
-        @Override
-        public void channelActive(ChannelHandlerContext ctx) {
-            File file = new File(filePath);
-            if (file.exists()) {
-                System.out.println("Sending file: " + filePath + " to peer " + peerIP);
-                // Senden der Datei und Hinzufügen eines ChannelFutureListeners
-                ctx.writeAndFlush(new DefaultFileRegion(file, 0, file.length())).addListener(new ChannelFutureListener() {
-                    @Override
-                    public void operationComplete(ChannelFuture future) {
-                        if (future.isSuccess()) {
-                            System.out.println("File sent successfully: " + filePath);
-                        } else {
-                            System.err.println("Error sending file: " + future.cause());
-                        }
-                        ctx.close(); // Schließen des Channels nach dem Senden
-                    }
-                });
-            } else {
-                System.err.println("File not found: " + filePath);
-                ctx.close();
-            }
-        }
-    
-        @Override
-        protected void channelRead0(ChannelHandlerContext ctx, Object msg) {
-            // Nicht benötigt in diesem Kontext
-            throw new UnsupportedOperationException("Unimplemented method 'channelRead0'");
-        }
-    }
 }
